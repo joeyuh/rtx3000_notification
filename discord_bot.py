4a64350d5908301d92c8b77269d3759ca99b7beb
Joe Yu <yuh22@kent-school.edu>
2020-11-16 23:09:25 -0500

    added discord uptime

--- discord_bot.py
+++ discord_bot.py
@@ -10,15 +10,39 @@ if os.path.exists('config.json'):
 else:
     raise Exception('MISSING config.json')
 
+UPTIME_NOTIFICATION_INTERVAL = 15
 TOKEN = settings['discord_token']
 GUILD = settings['discord_guild']
 LOCAL_TIME_ZONE = str(datetime.datetime.now().astimezone().tzinfo)
 
 client = discord.Client()
+start_time = datetime.datetime.now()
+alert_count = 0
 
 
-async def my_background_task():
+def strfdelta(tdelta, fmt):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return fmt.format(**d)
+
+
+async def uptime():
     await client.wait_until_ready()
+    global alert_count
+    while True:
+        for channel in client.get_all_channels():
+            if channel.name == 'uptime':
+                delta = datetime.datetime.now() - start_time
+                await channel.send(f'Bot is up! Last {UPTIME_NOTIFICATION_INTERVAL} minutes: {alert_count} alerts\n'
+                                   f'Total uptime {strfdelta(delta, "{days} days {hours} hours {minutes} minutes {seconds} seconds")}')
+                alert_count = 0
+        await asyncio.sleep(UPTIME_NOTIFICATION_INTERVAL * 60)
+
+
+async def alert_task():
+    await client.wait_until_ready()
+    global alert_count
 
     while True:
         links = []
@@ -30,6 +54,7 @@ async def my_background_task():
             for link in alert:
                 if not alert[link]['discord_alerted']:
                     links.append(link)  # We haven't alert, add to links
+                    alert_count += 1
                     alert[link]['discord_alerted'] = True  # We alerted
 
             for link in links:
@@ -53,7 +78,8 @@ async def on_ready():
 
 
 def main():
-    client.loop.create_task(my_background_task())
+    client.loop.create_task(uptime())
+    client.loop.create_task(alert_task())
     client.run(TOKEN)
 
 
