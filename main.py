78e74872421a9b2cbd78519cf6b13deefb0012af
Joe Yu <grandpajoe278@gmail.com>
2021-01-27 11:43:11 -0500

    add newegg cooldown

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ if __name__ == "__main__":
             discord_proc = Process(target=discord_bot.main)
             discord_proc.start()
 
+        newegg_last = datetime.datetime.now() - datetime.timedelta(days=1)
+        amazon_last = datetime.datetime.now() - datetime.timedelta(days=1)
+
         while True:  # Yes, while true
             start_time = time.time()
             v = Value('i', 0)  # success count
@@ -47,8 +50,26 @@ if __name__ == "__main__":
 
             best_buy_procs = [Process(target=bestbuy_detect, args=(url, v, l, lock, settings)) for url in
                               bestbuy_url_bank]
-            amazon_procs = [Process(target=amazon_detect, args=(url, v, l, lock, settings)) for url in amazon_url_bank]
-            newegg_procs = [Process(target=newegg_detect, args=(url, v, l, lock, settings)) for url in newegg_url_bank]
+            newegg_procs = []
+            wait = datetime.datetime.now() - newegg_last
+            if wait.total_seconds() > settings['newegg_cooldown_delay'] or \
+                    (not settings['newegg_cooldown']):
+                newegg_procs = [Process(target=newegg_detect, args=(url, v, l, lock, settings)) for url in
+                                newegg_url_bank]
+                newegg_last = datetime.datetime.now()
+            else:
+                print('Skipping newegg for cooldown')
+
+            wait = datetime.datetime.now() - amazon_last
+            amazon_procs = []
+            if wait.total_seconds() > settings['amazon_cooldown_delay'] or \
+                    (not settings['amazon_cooldown']):
+                amazon_procs = [Process(target=amazon_detect, args=(url, v, l, lock, settings)) for url in amazon_url_bank]
+                amazon_last = datetime.datetime.now()
+            else:
+                print('Skipping amazon for cooldown')
+
+            total_count = len(amazon_procs) + len(newegg_procs) + len(best_buy_procs)
 
             for p in best_buy_procs: p.start()
             for p in amazon_procs: p.start()
@@ -60,12 +81,12 @@ if __name__ == "__main__":
 
             if v.value == 0:
                 print(
-                 f'Detection of all the URL has failed. Sleeping for {settings["unknown_error_retry_delay"]} seconds!')
+                    f'Detection of all the URL has failed. Sleeping for {settings["unknown_error_retry_delay"]} seconds!')
                 print('Maybe check your Internet connection!')
                 time.sleep(settings["unknown_error_retry_delay"])
             else:
                 print(f'Went through all URLS in {time.time() - start_time} seconds, '
-                      f'{v.value} Success, {total_url_count - v.value} Failed')
+                      f'{v.value} Success, {total_count - v.value} Failed')
                 print("Links:")
                 print(l)
                 notify.notify(l)
